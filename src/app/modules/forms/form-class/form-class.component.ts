af285f732d53cc7e8b7638efb7f69c34953282ec
Rui Wang <rui.wang.624@gmail.com>
2021-02-08 23:31:44 -0500

    form class completed

--- src/app/modules/forms/form-class/form-class.component.ts
+++ src/app/modules/forms/form-class/form-class.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { formObj } from './models';
 
 @Component({
   selector: 'app-form-class',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormClassComponent implements OnInit {
 
+  formObj = formObj;
+  myForm = formObj.myForm;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.myForm.value);
+    console.log('Form submitted!');
   }
 
 }
--- src/app/modules/forms/form-class/models.ts
+++ src/app/modules/forms/form-class/models.ts
@@ -1,11 +1,38 @@
 import { FormControl, FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
+import { AbstractConstructor } from '@angular/material/core/common-behaviors/constructor';
 
 const fb = new FormBuilder();
 
-export const customer = new FormGroup({
-    name: new FormControl('', Validators.required),
-    stores: new FormGroup({})
-});
+export const formObj = {
+    myForm: fb.group({
+        name: [''],
+        stores: new FormArray([])
+    }),
 
+    get stores() {
+        return this.myForm.get('stores');
+    },
 
+    addStore: function() {
+        this.stores.push(fb.group({
+            storeName: [''],
+            purchases: new FormArray([])
+        }));
+        console.log('A store has been added.');
+    },
 
+    removeStore: function(i: number) {
+        this.stores.removeAt(i);
+    },
+
+    addPurchase: function(p: FormArray) {
+        p.push(fb.group({
+            item: [''],
+            price: [0]
+        }));
+    },
+
+    removePurchase: function(p: FormArray, i: number) {
+        p.removeAt(i);
+    }
+}
